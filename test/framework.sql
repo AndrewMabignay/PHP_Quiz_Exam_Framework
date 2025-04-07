304bc51363d0b42e3cf7027b9e6c177e77ed4006
AndrewMabignay <johnandrew.mabignay@gmail.com>
2025-04-07 09:34:00 +0800

    Update framework.sql

--- test/framework.sql
+++ test/framework.sql
@@ -14,8 +14,24 @@ CREATE TABLE CRUD (
     `Current_Time` TIME
 );
 
+-- ADD
 INSERT INTO CRUD (`FieldOne`, `FieldTwo`, `FieldThree`, `CountingField`, `Created_At`, `Current_Date`, `Current_Time`)
 VALUES
 ('Sample Data 1', 123, 'ValueOne', 10, CURRENT_TIMESTAMP, CURRENT_DATE, CURRENT_TIME),
 ('Sample Data 2', 456, 'ValueTwo', 20, CURRENT_TIMESTAMP, CURRENT_DATE, CURRENT_TIME),
 ('Sample Data 3', 789, 'ValueOne', 30, CURRENT_TIMESTAMP, CURRENT_DATE, CURRENT_TIME);
+
+-- RETRIEVE
+SELECT * FROM CRUD;
+
+-- EDIT
+UPDATE CRUD SET 
+    FieldOne = 'Data 1', 
+    FieldTwo = '1234', 
+    CountingField = '15', 
+    `Current_Date` = CURRENT_DATE, 
+    `Current_Time` = CURRENT_TIME
+WHERE ID = 1;
+
+-- DELETE
+DELETE FROM CRUD WHERE ID = 1;
